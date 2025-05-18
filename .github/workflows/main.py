68aa1bc277c3a24a6a7476d4c3c7a7adf6a21020
spark938 <sparkos.com@gmail.com>
2025-05-18 21:39:28 +0530

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -3,11 +3,13 @@ import requests
 
 def run():
     url = "https://nc-lotory.kesug.com/niyamitakelasa_browser_trigger.php"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122.0.0.0 Safari/537.36"
+    }
     while True:
-        print("Pinging the URL...")
         try:
-            res = requests.get(url)
-            print(f"Response: {res.status_code}")
+            response = requests.get(url, headers=headers)
+            print(f"[{time.ctime()}] Triggered: {response.status_code}")
         except Exception as e:
             print(f"Error: {e}")
         time.sleep(59)
